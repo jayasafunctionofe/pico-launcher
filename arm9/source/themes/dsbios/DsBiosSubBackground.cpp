938257a2d887507c09a2b520a2d11995dc4f0347
jayasafunctionofe <ja450n@gmail.com>
2026-05-10 00:10:31 -0400

    draw top bar

--- arm9/source/themes/dsbios/DsBiosSubBackground.cpp
+++ arm9/source/themes/dsbios/DsBiosSubBackground.cpp
@@ -1,9 +1,52 @@
 #include "common.h"
 #include <string.h>
 #include <nds/arm9/background.h>
+#include <nds/arm9/cache.h>
+#include <nds/system.h>
 #include "../ITheme.h"
 #include "DsBiosSubBackground.h"
 
+#include "DsFont.h"
+#include "DsIcons.h"
+
+
+
+
+// ########## BITMAP HELPERS ##########
+
+vu16* DsBiosSubBackground::BmpVram() const
+{
+    return reinterpret_cast<vu16*>(
+        reinterpret_cast<u8*>(BG_GFX_SUB) + 0x8000);
+}
+
+void DsBiosSubBackground::RestoreBgRegion(int x, int y, int w, int h)
+{
+    if (!_bgBuffer) return;
+
+    int x0 = x < 0 ? 0 : x;
+    int y0 = y < 0 ? 0 : y;
+    int x1 = x + w > 256 ? 256 : x + w;
+    int y1 = y + h > 192 ? 192 : y + h;
+
+    vu16* vram = BmpVram();
+
+    for (int row = y0; row < y1; row++) {
+        for (int col = x0; col < x1; col++) {
+            vram[row * 256 + col] = _bgBuffer[row * 256 + col];
+        }
+    }
+}
+
+void DsBiosSubBackground::DrawBmpPixel(int px, int py, u16 color)
+{
+    if (px < 0 || px >= 256 || py < 0 || py >= 192)
+        return;
+    BmpVram()[py * 256 + px] = color;
+}
+
+
+// ########## VBLANK ##########
 void DsBiosSubBackground::VBlank()
 {
     REG_DISPCNT_SUB = (REG_DISPCNT_SUB & ~0xF) | 5 | (4 << 8);
@@ -18,15 +61,19 @@ void DsBiosSubBackground::VBlank()
     REG_BG2PD_SUB = 256;
 }
 
+
+// ########## LOAD RESOURCES ##########
 void DsBiosSubBackground::LoadResources(const ITheme& theme, const VramContext& vramContext)
 {
-    auto tmpBuf = std::unique_ptr<u8[]>(new(cache_align) u8[256 * 192 * 2]);
+    _bgBuffer.reset(new(cache_align) u16[256 * 192]);
+    memset(_bgBuffer.get(), 0, 256 * 192 * sizeof(u16));
+
     const auto file = std::make_unique<File>();
+
     if (theme.OpenThemeFile(*file, "topbg.bin"))
     {
         u32 bytesRead = 0;
-        file->Read(tmpBuf.get(), 256 * 192 * 2, bytesRead);
-        memcpy((u8*)BG_GFX_SUB + 0x8000, tmpBuf.get(), 256 * 192 * 2);
+        file->Read(_bgBuffer.get(), 256 * 192 * sizeof(u16), bytesRead);
         file->Close();
     }
     
@@ -48,3 +95,77 @@ void DsBiosSubBackground::LoadResources(const ITheme& theme, const VramContext& 
 
     DrawTopBar();
 }
+
+// ########## UPDATE ##########
+void DsBiosSubBackground::Update()
+{
+
+}
+
+// ########## DRAW ##########
+void DsBiosSubBackground::Draw(GraphicsContext& graphicsContext)
+{
+    RestoreBgRegion(143, 0, 113, 16);
+
+    DrawTopBar();
+}
+
+
+
+
+// ########## TOP  BAR ##########
+
+void DsBiosSubBackground::DrawTopBar()
+{
+    DrawTopBarBackground();
+    DrawTopBarDividers();
+}
+
+
+void DsBiosSubBackground::DrawTopBarBackground()
+{
+    static constexpr int W = 256;
+
+    u16 colors[5] =
+    {
+        COL_OPAQUE | 0x0000, // 0 black
+        _palette.user.base,           // 1 base
+        _palette.user.light1,        // 2
+        _palette.user.light2,        // 3
+        _palette.user.light3,        // 4
+    };
+
+    static constexpr u8 pattern[16][2] =
+    {
+        {4,4}, {3,4}, {4,3}, {3,3},
+        {2,3}, {3,2}, {2,2}, {2,2},
+        {2,1}, {1,2}, {1,1}, {1,1},
+        {1,1}, {1,1}, {1,1}, {0,0}
+    };
+
+    for (int y = 0; y < 16; y++)
+    {
+        for (int x = 0; x < W; x++)
+        {
+            DrawBmpPixel(x, y, colors[pattern[y][x & 1]]);
+        }
+    }
+}
+
+void DsBiosSubBackground::DrawTopBarDividers()
+{
+    static constexpr int xs[] = {143, 175, 207, 223, 239};
+
+    for (int x : xs)
+    {
+        for (int y = 0; y < 15; y++)
+        {
+            // pattern: 110 (draw, draw, skip)
+            if ((y % 3) != 2)
+            {
+                DrawBmpPixel(x, y, _palette.darkGray);
+            }
+        }
+    }
+}
+
--- arm9/source/themes/dsbios/DsBiosSubBackground.h
+++ arm9/source/themes/dsbios/DsBiosSubBackground.h
@@ -2,11 +2,13 @@
 #include "../background/IThemeBackground.h"
 #include "../IFontRepository.h"
 
+#include <memory>
 #include "DsBiosSystemInfo.h"
 #include "DsColorUtils.h"
+#include <nds/system.h>
+
 #include "DsFont.h"
 #include "DsIcons.h"
-
 
 class DsBiosSubBackground : public IThemeBackground
 {
@@ -25,8 +27,10 @@ public:
     void Draw(GraphicsContext& graphicsContext) override;
     void VBlank() override;
 
+    // Bitmap helper - public
+    void DrawBmpPixel(int px, int py, u16 color);
+
 private:
-;
     const IFontRepository* _fontRepository;
 
     DsBiosSystemInfo::Settings _systemSettings;
@@ -35,6 +39,42 @@ private:
 
     // get a palette we can use for objects that
     // change based on user's favorite color
-    UiPalette _palette = MakeUiPalette(THEME_USER_PALETTES[0]);
+    UiPalette _palette = MakeUiPalette(THEME_USER_PALETTES[0]);    
+
+    // RAM copy of the unmodified background for region restoration
+    std::unique_ptr<u16[]> _bgBuffer; // 256 * 192 u16s = 98 304 bytes
     
+    // topbar
+    static constexpr int TB_TEXT_Y_POS = 3;
+    static constexpr int TB_USER_X_POS = 8;
+    static constexpr int TB_CLOCK_X_POS = 148;
+    static constexpr int TB_DATE_X_POS = 178;
+
+    // analogue clock
+    static constexpr int CLOCK_CX     = 63;
+    static constexpr int CLOCK_CY     = 95;
+    static constexpr int CLOCK_W      = 96;
+    
+    static constexpr int HOUR_LEN     = 24;
+    static constexpr int MIN_LEN      = 32;
+    static constexpr int SEC_LEN      = 36;
+    static constexpr int ALARM_LEN    = 15;
+    static constexpr int CLOCK_RESTORE_R = SEC_LEN + 2;
+
+    // calendar
+    static constexpr int CAL_ORIGIN_X = 128;
+    static constexpr int CAL_ORIGIN_Y = 64;
+    static constexpr int CAL_CELL_W   = 16;
+    static constexpr int CAL_CELL_H   = 16;
+
+    // Bitmap helpers 
+    vu16* BmpVram() const;
+    void RestoreBgRegion(int x, int y, int w, int h);
+    void DrawIndexedIcon(int x, int y, const u8* icon, int w, int h, const u16 palette[4]);
+
+    // Top Bar stuff
+    void DrawTopBar();
+    void DrawTopBarBackground();
+    void DrawTopBarDividers();
+
 };
